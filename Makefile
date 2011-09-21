38b70ec112202d5ace5f89ada1ae4c2d349b9bb0
Christian Speckner <Christian.Speckner@physik.uni-freiburg.de>
2011-09-21 10:32:11 +0200

    Properly destroy pixels on exit.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = g++
-CXXFLAGS = -O2 -march=native -mno-avx
+CXXFLAGS = -O2 -march=native -mno-avx -g
 LDFLAGS = 
 
 CXXFLAGS += $(shell sdl-config --cflags)
--- global.cc
+++ global.cc
@@ -5,12 +5,13 @@
 #include "screenmessage.h"
 
 GlobalData::GlobalData () : setup(0), viewport(0), messages(0), rng(0),
-   dispatcher(0)
+   dispatcher(0), pixels(0)
 {}
 
 GlobalData::~GlobalData () {
    if (setup) delete setup;
    if (viewport) delete viewport;
+   if (pixels) delete pixels;
    if (messages) delete messages;
    if (rng) delete rng;
    if (dispatcher) delete dispatcher;
